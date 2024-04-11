638182d0e9db968a84a3573eef812e685d50bf96
Riv33R <dimageht69@gmail.com>
2024-04-11 21:57:09 +0300

    !

--- app.py
+++ app.py
@@ -8,32 +8,39 @@ import pygame
 
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'static/audio'  # Путь для сохранения аудио файлов
-app.config['ALLOWED_EXTENSIONS'] = {'mp3', 'wav', 'ogg'}
-app.secret_key = 'your_secret_key'
-schedule_file = 'schedule.json'  # Путь к файлу с расписанием
+app.config["UPLOAD_FOLDER"] = "static/audio"  # Путь для сохранения аудио файлов
+app.config["ALLOWED_EXTENSIONS"] = {"mp3", "wav", "ogg"}
+app.secret_key = "your_secret_key"
+schedule_file = "schedule.json"  # Путь к файлу с расписанием
 
 played_slots = {}
 
-UPLOAD_FOLDER = 'static/audio'
+UPLOAD_FOLDER = "static/audio"
 
 # Создание папок для загрузки файлов, если они еще не существуют
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
+
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+    return (
+        "." in filename
+        and filename.rsplit(".", 1)[1].lower() in app.config["ALLOWED_EXTENSIONS"]
+    )
+
 
 def save_schedule(schedule):
-    with open(schedule_file, 'w') as f:
+    with open(schedule_file, "w") as f:
         json.dump(schedule, f)
+
 
 def load_schedule():
     try:
-        with open(schedule_file, 'r') as f:
+        with open(schedule_file, "r") as f:
             return json.load(f)
     except FileNotFoundError:
         return []
+
 
 def play_audio_once(file_path):
     """Воспроизведение аудиофайла с ожиданием его окончания."""
@@ -44,136 +51,159 @@ def play_audio_once(file_path):
     pygame.mixer.music.stop()
     pygame.mixer.music.unload()
 
+
 def play_scheduled_audio():
     pygame.mixer.init()
     while True:
-        now = datetime.now().strftime('%H:%M')
+        now = datetime.now().strftime("%H:%M")
         schedule = load_schedule()
         for index, slot in enumerate(schedule):
-            lesson_start = slot['lesson_start']
-            lesson_end = slot['lesson_end']
-            audio_file_path = os.path.join(app.config['UPLOAD_FOLDER'], slot['audio_file_path'])
+            lesson_start = slot["lesson_start"]
+            lesson_end = slot["lesson_end"]
+            audio_file_path = os.path.join(
+                app.config["UPLOAD_FOLDER"], slot["audio_file_path"]
+            )
 
             # Воспроизведение в начале урока
-            if now == lesson_start and not played_slots.get(index, {}).get('start', False):
+            if now == lesson_start and not played_slots.get(index, {}).get(
+                "start", False
+            ):
                 play_audio_once(audio_file_path)
                 if index not in played_slots:
                     played_slots[index] = {}
-                played_slots[index]['start'] = True  # Отмечаем, что воспроизведение на начало занятия выполнено
+                played_slots[index][
+                    "start"
+                ] = True  # Отмечаем, что воспроизведение на начало занятия выполнено
 
             # Воспроизведение в конце урока
-            elif now == lesson_end and not played_slots.get(index, {}).get('end', False):
+            elif now == lesson_end and not played_slots.get(index, {}).get(
+                "end", False
+            ):
                 play_audio_once(audio_file_path)
                 if index not in played_slots:
                     played_slots[index] = {}
-                played_slots[index]['end'] = True  # Отмечаем, что воспроизведение на окончание занятия выполнено
+                played_slots[index][
+                    "end"
+                ] = True  # Отмечаем, что воспроизведение на окончание занятия выполнено
 
         time.sleep(2)  # Периодическая проверка расписания
 
 
-@app.route('/')
+@app.route("/")
 def index():
     schedule = load_schedule()
-    return render_template('index.html', schedule=schedule)
+    return render_template("index.html", schedule=schedule)
 
-@app.route('/add', methods=['POST'])
+
+@app.route("/add", methods=["POST"])
 def add_time_slot():
     schedule = load_schedule()
-    if request.method == 'POST':
-        lesson_start = request.form['lesson_start']
-        lesson_end = request.form['lesson_end']
-        audio_file = request.files['audio_file']
+    if request.method == "POST":
+        lesson_start = request.form["lesson_start"]
+        lesson_end = request.form["lesson_end"]
+        audio_file = request.files["audio_file"]
 
         if audio_file and allowed_file(audio_file.filename):
-            filename = secure_filename(audio_file.filename) # type: ignore
-            audio_file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            filename = secure_filename(audio_file.filename)  # type: ignore
+            audio_file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
             audio_file.save(audio_file_path)
-            schedule.append({
-                'lesson_start': lesson_start,
-                'lesson_end': lesson_end,
-                'audio_file_path': filename
-            })
+            schedule.append(
+                {
+                    "lesson_start": lesson_start,
+                    "lesson_end": lesson_end,
+                    "audio_file_path": filename,
+                }
+            )
             save_schedule(schedule)
-            flash('Новый звонок успешно добавлен.', 'success')
+            flash("Новый звонок успешно добавлен.", "success")
         else:
-            flash('Ошибка добавления: неверный формат файла.', 'error')
+            flash("Ошибка добавления: неверный формат файла.", "error")
 
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
 
-@app.route('/delete/<int:index>', methods=['POST'])
+@app.route("/delete/<int:index>", methods=["POST"])
 def delete_time_slot(index):
     schedule = load_schedule()
     try:
-        audio_file_path = schedule[index]['audio_file_path']
-        if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], audio_file_path)):
-            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], audio_file_path))
+        audio_file_path = schedule[index]["audio_file_path"]
+        if os.path.exists(os.path.join(app.config["UPLOAD_FOLDER"], audio_file_path)):
+            os.remove(os.path.join(app.config["UPLOAD_FOLDER"], audio_file_path))
         del schedule[index]
         save_schedule(schedule)
-        flash('Звонок успешно удалён.', 'success')
+        flash("Звонок успешно удалён.", "success")
     except (IndexError, FileNotFoundError):
-        flash('Ошибка удаления: звонок не найден.', 'error')
+        flash("Ошибка удаления: звонок не найден.", "error")
 
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-@app.route('/edit_time/<int:index>', methods=['POST'])
+
+@app.route("/edit_time/<int:index>", methods=["POST"])
 def edit_time(index):
     schedule = load_schedule()
-    if request.method == 'POST':
+    if request.method == "POST":
         try:
-            schedule[index]['lesson_start'] = request.form['lesson_start']
-            schedule[index]['lesson_end'] = request.form['lesson_end']
+            schedule[index]["lesson_start"] = request.form["lesson_start"]
+            schedule[index]["lesson_end"] = request.form["lesson_end"]
             save_schedule(schedule)
-            flash('Время звонка успешно изменено.', 'success')
+            flash("Время звонка успешно изменено.", "success")
         except IndexError:
-            flash('Ошибка изменения: звонок не найден.', 'error')
+            flash("Ошибка изменения: звонок не найден.", "error")
 
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-@app.route('/replace_audio/<int:index>', methods=['POST'])
+
+@app.route("/replace_audio/<int:index>", methods=["POST"])
 def replace_audio(index):
     schedule = load_schedule()
-    if request.method == 'POST':
-        audio_file = request.files['audio_file']
+    if request.method == "POST":
+        audio_file = request.files["audio_file"]
         if audio_file and allowed_file(audio_file.filename):
-            filename = secure_filename(audio_file.filename) # type: ignore
-            old_file_path = schedule[index]['audio_file_path']
-            if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], old_file_path)):
-                os.remove(os.path.join(app.config['UPLOAD_FOLDER'], old_file_path))
-            audio_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            schedule[index]['audio_file_path'] = filename
+            filename = secure_filename(audio_file.filename)  # type: ignore
+            old_file_path = schedule[index]["audio_file_path"]
+            if os.path.exists(os.path.join(app.config["UPLOAD_FOLDER"], old_file_path)):
+                os.remove(os.path.join(app.config["UPLOAD_FOLDER"], old_file_path))
+            audio_file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            schedule[index]["audio_file_path"] = filename
             save_schedule(schedule)
-            flash('Аудио файл успешно заменён.', 'success')
+            flash("Аудио файл успешно заменён.", "success")
         else:
-            flash('Ошибка замены аудио: неверный формат файла.', 'error')
+            flash("Ошибка замены аудио: неверный формат файла.", "error")
 
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-@app.route('/move_up/<int:index>', methods=['POST'])
+
+@app.route("/move_up/<int:index>", methods=["POST"])
 def move_up(index):
     schedule = load_schedule()  # Загружаем расписание
-    if index > 0 and index < len(schedule):  # Убедимся, что индекс в допустимом диапазоне
+    if index > 0 and index < len(
+        schedule
+    ):  # Убедимся, что индекс в допустимом диапазоне
         # Меняем элементы местами
         schedule[index], schedule[index - 1] = schedule[index - 1], schedule[index]
         save_schedule(schedule)  # Сохраняем изменённое расписание
     else:
-        flash('Ошибка перемещения: некорректный индекс.', 'error')
-    return redirect(url_for('index'))  # Используем маршрут 'index' для перенаправления
+        flash("Ошибка перемещения: некорректный индекс.", "error")
+    return redirect(url_for("index"))  # Используем маршрут 'index' для перенаправления
 
-@app.route('/move_down/<int:index>', methods=['POST'])
+
+@app.route("/move_down/<int:index>", methods=["POST"])
 def move_down(index):
     schedule = load_schedule()  # Загружаем расписание
-    if index >= 0 and index < len(schedule) - 1:  # Убедимся, что индекс в допустимом диапазоне
+    if (
+        index >= 0 and index < len(schedule) - 1
+    ):  # Убедимся, что индекс в допустимом диапазоне
         # Меняем элементы местами
         schedule[index], schedule[index + 1] = schedule[index + 1], schedule[index]
         save_schedule(schedule)  # Сохраняем изменённое расписание
     else:
-        flash('Ошибка перемещения: некорректный индекс.', 'error')
-    return redirect(url_for('index'))  # Используем маршрут 'index' для перенаправления
+        flash("Ошибка перемещения: некорректный индекс.", "error")
+    return redirect(url_for("index"))  # Используем маршрут 'index' для перенаправления
+
 
 from threading import Thread
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     audio_thread = Thread(target=play_scheduled_audio)
     audio_thread.start()
     app.run(debug=False)
